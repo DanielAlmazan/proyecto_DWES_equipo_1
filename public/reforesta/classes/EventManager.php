0be20139c9578982f0c8a410c2565d3a4f8dcbe5
LiamAsensi <liam.alejoasensi@gmail.com>
2023-11-15 23:09:53 +0100

    Adding the EventManager class and adding almost all functionality

---
+++ public/reforesta/classes/EventManager.php
@@ -0,0 +1,48 @@
+<?php
+    class EventManager {
+        private array $events;
+        
+        public function __construct(array $events)
+        {
+            $this->events = $events;
+        }
+
+        public function __get(string $property) {
+            switch($property) {
+                case "events":
+                    return $this->events;
+            }
+        }
+
+        public function __set(string $property, $value) {
+            switch($property) {
+                case "events":
+                    $this->events = $value;
+                    break;
+            }
+        }
+
+        public function addEvent(Event $newEvent): bool {
+            $this->events[] = $newEvent;
+            return true;
+        }
+
+        public function filterString(string $field, string $value): array {
+            return array_filter($this->events, function($event) use ($field, $value) {
+                return $event->$field === $value;
+            });
+        }
+
+        public function filterDate(DateTime $date) {
+            return array_filter($this->events, function($event) use ($date) {
+                return $event->date === $date;
+            });
+        }
+
+        /*
+        TODO 
+        - Filter by host once the User class is implemented
+        - Filter by approval
+        */
+    }
+?>
