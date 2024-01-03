5f0086a1d9e149576b2d7a465be5f41cffd1a79a
DanielAlmazan <almazansellesdaniel@gmail.com>
2024-01-03 17:35:24 +0100

    The __get() and __set() magic methods in the Specie class have been replaced with specific getters and setters because the client doesn't know what she wants

--- public/reforesta/classes/Specie.php
+++ public/reforesta/classes/Specie.php
@@ -32,30 +32,71 @@
             $this->url = $url;
         }
 
-    public function __get(string $field){
-        switch ($field){
-            case "id":
-                return $this->id;
-            case "scientificName":
-                return $this->scientificName;
-            case "commonName":
-                return $this->commonName;
-            case "climate":
-                return $this->climate;
-            case "region":
-                return $this->region;
-            case "daysToGrow":
-                return $this->daysToGrow;
-            case "benefits":
-                return $this->benefits;
-            case "picture":
-                return $this->picture;
-            case "url":
-                return $this->url;
-            default:
-                return "Error";
+        public function getId(): int {
+            return $this->id;
+        }
+
+        public function getScientificName(): string {
+            return $this->scientificName;
+        }
+
+        public function setScientificName(string $scientificName): void {
+            $this->scientificName = $scientificName;
+        }
+
+        public function getCommonName(): string {
+            return $this->commonName;
+        }
+
+        public function setCommonName(string $commonName): void {
+            $this->commonName = $commonName;
+        }
+
+        public function getClimate(): string {
+            return $this->climate;
+        }
+
+        public function setClimate(string $climate): void {
+            $this->climate = $climate;
+        }
+
+        public function getRegion(): string {
+            return $this->region;
+        }
+
+        public function setRegion(string $region): void {
+            $this->region = $region;
+        }
+
+        public function getDaysToGrow(): int {
+            return $this->daysToGrow;
+        }
+
+        public function setDaysToGrow(int $daysToGrow): void {
+            $this->daysToGrow = $daysToGrow;
+        }
+
+        public function getBenefits(): array {
+            return $this->benefits;
+        }
+
+        public function setBenefits(array $benefits): void {
+            $this->benefits = $benefits;
+        }
+
+        public function getPicture(): string {
+            return $this->picture;
+        }
+
+        public function setPicture(string $picture): void {
+            $this->picture = $picture;
+        }
+
+        public function getUrl(): string {
+            return $this->url;
+        }
+
+        public function setUrl(string $url): void {
+            $this->url = $url;
         }
     }
-
-
-}
