6968e993710197313c5fc9074002c934e9732dc2
DanielAlmazan <almazansellesdaniel@gmail.com>
2024-01-03 17:35:04 +0100

    Reformatted Specie class structure

--- public/reforesta/classes/Specie.php
+++ public/reforesta/classes/Specie.php
@@ -1,38 +1,36 @@
 <?php
 
-class Specie
-{
-    private int $id;
-    private string $scientificName;
-    private string $commonName;
-    private string $climate;
-    private string $region;
-    private int $daysToGrow;
-    private array $benefits;
-    private string $picture;
-    private string $url;
+    class Specie {
+        private int $id;
+        private string $scientificName;
+        private string $commonName;
+        private string $climate;
+        private string $region;
+        private int $daysToGrow;
+        private array $benefits;
+        private string $picture;
+        private string $url;
 
-    public function __construct(
-        int $id,
-        string $scientificName,
-        string $commonName,
-        string $climate,
-        string $region,
-        int $daysToGrow,
-        array $benefits,
-        string $picture,
-        string $url)
-    {
-        $this->id = $id;
-        $this->scientificName = $scientificName;
-        $this->commonName = $commonName;
-        $this->climate = $climate;
-        $this->region = $region;
-        $this->daysToGrow = $daysToGrow;
-        $this->benefits = $benefits;
-        $this->picture = $picture;
-        $this->url = $url;
-    }
+        public function __construct(
+            int    $id,
+            string $scientificName,
+            string $commonName,
+            string $climate,
+            string $region,
+            int    $daysToGrow,
+            array  $benefits,
+            string $picture,
+            string $url) {
+            $this->id = $id;
+            $this->scientificName = $scientificName;
+            $this->commonName = $commonName;
+            $this->climate = $climate;
+            $this->region = $region;
+            $this->daysToGrow = $daysToGrow;
+            $this->benefits = $benefits;
+            $this->picture = $picture;
+            $this->url = $url;
+        }
 
     public function __get(string $field){
         switch ($field){
