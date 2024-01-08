7a198ff7c8f921671f9a9894cbe09ec6aeb18307
DanielAlmazan <almazansellesdaniel@gmail.com>
2024-01-08 20:08:38 +0100

    Moved from "root" to res directory

--- public/reforesta/res/js/jquery.magnific-popup.js
+++ public/reforesta/res/js/jquery.magnific-popup.js
@@ -4,10 +4,10 @@
 ;(function (factory) { 
 if (typeof define === 'function' && define.amd) { 
  // AMD. Register as an anonymous module. 
- define(['jquery'], factory); 
+ define(['./jquery'], factory);
  } else if (typeof exports === 'object') { 
  // Node/CommonJS 
- factory(require('jquery')); 
+ factory(require('./jquery'));
  } else { 
  // Browser globals 
  factory(window.jQuery || window.Zepto); 
