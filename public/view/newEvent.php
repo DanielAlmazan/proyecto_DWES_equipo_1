2520ade582f3f462dedbc7e0a7f58a5a15cd1055
LiamAsensi <liam.alejoasensi@gmail.com>
2024-01-09 00:48:38 +0100

    Added the new Event page

---
+++ public/view/newEvent.php
@@ -0,0 +1,16 @@
+<?php
+$pageTitle = "Add Event";
+require_once("header.php");
+?>
+<main>
+    <form action="">
+        <label>
+            Name
+            <input type="text" name="name" id="name" placeholder="Enter the event's name">
+        </label>
+    </form>
+</main>
+
+<?php
+require_once('footer.php');
+?>
