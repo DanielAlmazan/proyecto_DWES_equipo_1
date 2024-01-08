59f22925790083c16927ec5729684fe7ab913422
LiamAsensi <liam.alejoasensi@gmail.com>
2024-01-08 23:59:29 +0100

    Added Router and its uses

--- public/index.php
+++ public/index.php
@@ -1,4 +1,35 @@
 <?php
-    header("Location: view/home.php");
-    // TODO: Implement router 🙃
+require 'Router.php';
+
+$router = new Router();
+
+$router->add('/', function () {
+    header('Location: /view/home.php');
+});
+$router->add('/home', function () {
+    header('Location: /view/home.php');
+});
+$router->add('/about', function () {
+    header('Location: /view/about.php');
+});
+$router->add('/login', function () {
+    header('Location: /view/login.php');
+});
+$router->add('/register', function () {
+    header('Location: /view/register.php');
+});
+$router->add('/species', function () {
+    header('Location: /view/our_species.php');
+});
+$router->add('/contact', function () {
+    header('Location: /view/contact.php');
+});
+$router->add('/achievements', function () {
+    header('Location: /view/achievements.php');
+});
+$router->add('/blog', function () {
+    header('Location: /view/blog.php');
+});
+
+$router->run();
 ?>
