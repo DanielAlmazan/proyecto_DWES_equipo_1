8f859ad79302cd9f0519cb1563cda8f1e3f512b7
cristian <cristian2306c@gmail.com>
2024-01-21 01:19:28 +0100

    Added getSpeciesList() methond but still on progress to make it work

--- public/controller/SpeciesController.php
+++ public/controller/SpeciesController.php
@@ -1,7 +1,23 @@
 <?php
 
-    namespace controller;
+namespace controller;
 
-    class SpeciesController {
+use Specie;
+use DB\ReforestaDB;
 
+class SpeciesController {
+
+    private array $species;
+
+    public function __construct() {
+        
     }
+
+     function getSpeciesList() {
+        $species=Specie::getSpecies();
+        require_once dirname(__DIR__).'/view/our_species.php';
+        
+    }
+
+
+}
