e6e1ddb58de160ec9da0ddaf40d209c5ea00e168
Miguel <ayorino23@hotmail.com>
2024-03-05 19:37:21 +0100

    Adding try-catch clauses

--- public/model/User.php
+++ public/model/User.php
@@ -72,6 +72,7 @@
 
 	    public function setKarma(int $karma): void {
 		    $this->karma = $karma;
+			$this->updateKarma();
 	    }
 
 	    public function getAvatar(): string {
@@ -83,147 +84,219 @@
 	    }
 
         function insert() {
-            $pdo = ReforestaDB::connectDB();
-            $sql = "INSERT INTO users (name, surnames, email, nickName, password, avatar)" .
-				" VALUES (:name, :surnames, :email, :nickName, :password, :avatar)";
-            $insert = $pdo->prepare($sql);
+			$correct = false;
 
-			// Bind params
-            $insert->bindParam(":name", $this->name);
-            $insert->bindParam(":surnames", $this->surnames);
-            $insert->bindParam(":email", $this->email);
-            $insert->bindParam(":nickName", $this->nickName);
-			$insert->bindParam(":password", $this->password);
-			$insert->bindParam(":avatar", $this->avatar);
-			
-            $insert->execute();
-			$pdo = null;
+			try {
+				$pdo = ReforestaDB::connectDB();
+				$sql = "INSERT INTO users (name, surnames, email, nickName, password, avatar)" .
+					" VALUES (:name, :surnames, :email, :nickName, :password, :avatar)";
+				$insert = $pdo->prepare($sql);
+
+				// Bind params
+				$insert->bindParam(":name", $this->name);
+				$insert->bindParam(":surnames", $this->surnames);
+				$insert->bindParam(":email", $this->email);
+				$insert->bindParam(":nickName", $this->nickName);
+				$insert->bindParam(":password", $this->password);
+				$insert->bindParam(":avatar", $this->avatar);
+				
+				$correct = $insert->execute();
+			} catch(Exception $e) {
+				echo "<p class='error'>" . $e->getMessage(). "</p>";
+			} finally {
+				$insert = null;
+				$pdo = null;
+			}
+            
+			return $correct;
         }
 		
 		function update() {
-			$pdo = ReforestaDB::connectDB();
-			$sql = "UPDATE users SET name=:name, surnames=:surnames, email=:email, nickName=:nickName, password=:password, avatar=:avatar WHERE id=:id";
-			$update = $pdo->prepare($sql);
+			$correct = false;
+			
+			try {
+				$pdo = ReforestaDB::connectDB();
+				$sql = "UPDATE users SET name=:name, surnames=:surnames, email=:email, nickName=:nickName, password=:password, avatar=:avatar WHERE id=:id";
+				$update = $pdo->prepare($sql);
 
-			// Bind params
-			$update->bindParam(":name", $this->name);
-			$update->bindParam(":surnames", $this->surnames);
-			$update->bindParam(":email", $this->email);
-			$update->bindParam(":nickName", $this->nickName);
-			$update->bindParam(":password", $this->password);
-			$update->bindParam(":avatar", $this->avatar);
-			$update->bindParam(":id", $this->id);
+				// Bind params
+				$update->bindParam(":name", $this->name);
+				$update->bindParam(":surnames", $this->surnames);
+				$update->bindParam(":email", $this->email);
+				$update->bindParam(":nickName", $this->nickName);
+				$update->bindParam(":password", $this->password);
+				$update->bindParam(":avatar", $this->avatar);
+				$update->bindParam(":id", $this->id);
 
-			$update->execute();
-			$pdo = null;
+				$correct = $update->execute();
+			} catch(Exception $e) {
+				echo "<p class='error'>" . $e->getMessage(). "</p>";
+			} finally {
+				$update = null;
+				$pdo = null;
+			}
+			
+			return $correct;
 		}
 
-		function updateKarma() {
-			$pdo = ReforestaDB::connectDB();
-			$sql = "UPDATE users SET karma=:karma WHERE id=:id";
-			$update = $pdo->prepare($sql);
+		private function updateKarma() {
+			try {
+				$pdo = ReforestaDB::connectDB();
+				$sql = "UPDATE users SET karma=:karma WHERE id=:id";
+				$update = $pdo->prepare($sql);
 
-			// Bind params
-			$update->bindParam(":karma", $this->karma);
-			$update->bindParam(":id", $this->id);
+				// Bind params
+				$update->bindParam(":karma", $this->karma);
+				$update->bindParam(":id", $this->id);
 
-			$update->execute();
-			$pdo = null;
+				$update->execute();
+			} catch(Exception $e) {
+				echo "<p class='error'>" . $e->getMessage(). "</p>";
+			} finally {
+				$update = null;
+				$pdo = null;
+			}
 		}
 		
 		function delete() {
-			$pdo = ReforestaDB::connectDB();
-			$sql = "DELETE FROM users WHERE id=:id";
-			$delete = $pdo->prepare($sql);
+			$correct = false;
 
-			// Bind params
-			$delete->bindParam(":id", $id);
+			try {
+				$pdo = ReforestaDB::connectDB();
+				$sql = "DELETE FROM users WHERE id=:id";
+				$delete = $pdo->prepare($sql);
 
-			$delete->execute();
-			$pdo = null;
+				// Bind params
+				$delete->bindParam(":id", $id);
+
+				$correct = $delete->execute();
+			} catch(Exception $e) {
+				echo "<p class='error'>" . $e->getMessage(). "</p>";
+			} finally {
+				$delete = null;
+				$pdo = null;
+			}
+
+			return $correct;
 		}
 
 		static function getAll() {
-            $pdo = ReforestaDB::connectDB();
-            $sql = "SELECT * FROM users";
-            $users = [];
+			$users = [];
 
-            $select = $pdo->query($sql);
-            while ($user = $select->fetchobject()) {
-                $users[] = new User(
-					$user->name,
-					$user->surenames,
-					$user->email,
-					$user->nickName,
-					$user->password,
-					$user->avatar,
-					$user->karma,
-					$user->id
-                );
-            }
+			try {
+				$pdo = ReforestaDB::connectDB();
+				$sql = "SELECT * FROM users";
 
-			$pdo = null;
+				$select = $pdo->query($sql);
+				$select->execute();
+				while ($user = $select->fetchobject()) {
+					$users[] = new User(
+						$user->name,
+						$user->surenames,
+						$user->email,
+						$user->nickName,
+						$user->password,
+						$user->avatar,
+						$user->karma,
+						$user->id
+					);
+				}
+			} catch(Exception $e) {
+				echo "<p class='error'>" . $e->getMessage(). "</p>";
+			} finally {
+				$select = null;
+				$pdo = null;
+			}
+
             return $users;
         }
 		
 		static function getById(int $id) {
-			$pdo = ReforestaDB::connectDB();
-			$sql = "SELECT * FROM users WHERE id=:id";
-			$select = $pdo->prepare($sql);
+			$user = null;
 
-			// Bind params
-			$select->bindParam(":id", $id);
+			try {
+				$pdo = ReforestaDB::connectDB();
+				$sql = "SELECT * FROM users WHERE id=:id";
+				$select = $pdo->prepare($sql);
 
-			$select->execute();
-			if($result = $select->fetchObject()) {
-				$user = new User(
-					$result->name, 
-					$result->surnames, 
-					$result->email, 
-					$result->nickName,
-					$result->password, 
-					$result->avatar,
-					$result->karma,
-					$result->id
-				);
-			} else $user = null;
+				// Bind params
+				$select->bindParam(":id", $id);
 
-			$pdo = null;
+				$select->execute();
+				if($result = $select->fetchObject()) {
+					$user = new User(
+						$result->name, 
+						$result->surnames, 
+						$result->email, 
+						$result->nickName,
+						$result->password, 
+						$result->avatar,
+						$result->karma,
+						$result->id
+					);
+				}
+			} catch(Exception $e) {
+				echo "<p class='error'>" . $e->getMessage(). "</p>";
+			} finally {
+				$select = null;
+				$pdo = null;
+			}
+
 			return $user;
 		}
 
 		static function suscribeNewsletter(string $email) {
-            $pdo = ReforestaDB::connectDB();
-            $sql = "INSERT INTO newsletterSubscribers (:email)";
-            $insert = $pdo->prepare($sql);
+			$correct = false;
 
-			// Bind params
-            $insert->bindParam(":email", $email);
-			
-            $insert->execute();
-			$pdo = null;
+			try {
+				$pdo = ReforestaDB::connectDB();
+				$sql = "INSERT INTO newsletterSubscribers (:email)";
+				$insert = $pdo->prepare($sql);
+
+				// Bind params
+				$insert->bindParam(":email", $email);
+				
+				$correct = $insert->execute();
+			} catch(Exception $e) {
+				echo "<p class='error'>" . $e->getMessage(). "</p>";
+			} finally {
+				$insert = null;
+				$pdo = null;
+			}
+
+			return $correct;
         }
 
 		static function unsuscribeNewsletter(string $email) {
-            $pdo = ReforestaDB::connectDB();
-			$sql = "SELECT * FROM newsletterSubscribers WHERE email=:email";
-			$select = $pdo->prepare($sql);
+			$correct = false;
 
-			// Bind params
-			$select->bindParam(":email", $email);
-
-			$select->execute();
-			if($select->fetchObject()) {
-				$sql = "DELETE FROM newsletterSubscribers WHERE email=:email";
-				$delete = $pdo->prepare($sql);
+			try {
+				$pdo = ReforestaDB::connectDB();
+				$sql = "SELECT * FROM newsletterSubscribers WHERE email=:email";
+				$select = $pdo->prepare($sql);
 
 				// Bind params
-				$delete->bindParam(":email", $email);;
+				$select->bindParam(":email", $email);
 
-				$delete->execute();
+				$select->execute();
+				if($select->fetchObject()) {
+					$sql = "DELETE FROM newsletterSubscribers WHERE email=:email";
+					$delete = $pdo->prepare($sql);
+
+					// Bind params
+					$delete->bindParam(":email", $email);;
+
+					$correct = $delete->execute();
+				}
+			} catch(Exception $e) {
+				echo "<p class='error'>" . $e->getMessage(). "</p>";
+			} finally {
+				$select = null;
+				$delete = null;
+				$pdo = null;
 			}
-            
-			$pdo = null;
+
+			return $correct;
         }
     }
 ?>
