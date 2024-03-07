c5f34a76c42e422c4ea675cc2cf80632a5c2e74c
DanielAlmazan <almazansellesdaniel@gmail.com>
2024-03-07 14:14:56 +0100

    Finished login functionality.

--- public/view/login.php
+++ public/view/login.php
@@ -1,11 +1,26 @@
 <?php
+    if (!empty($_POST["email"]) && !empty($_POST["pass"])) {
+        require_once("../model/User.php");
+        require_once("../DB/ReforestaDB.php");
+        
+        $email = $_POST["email"];
+        $password = $_POST["pass"];
+        $user = User::getByLogin($email, $password);
+        
+        if ($user) {
+            $_SESSION["userId"] = $user->getId();
+            header("Location: home.php");
+        } else {
+            echo "Invalid email or password";
+        }
+    }
     $pageTitle = "Login";
     require_once("header.php");
 ?>
 
 <h1>LOGIN</h1>
 
-<form action="#" method="post" id="registerForm" class="form">
+<form action="<?= $_SERVER['PHP_SELF'] ?>" method="post" id="registerForm" class="form">
     <label>Email
         <input type="text" name="email" placeholder="Write your email...">
     </label>
