725370980f16f20d3b3306ab722f6c00c59bb475
DanielAlmazan <almazansellesdaniel@gmail.com>
2024-03-07 14:11:04 +0100

    Fixed 'name' properties. Yes, I forgot to write them.

--- public/view/login.php
+++ public/view/login.php
@@ -7,10 +7,10 @@
 
 <form action="#" method="post" id="registerForm" class="form">
     <label>Email
-        <input type="text">
+        <input type="text" name="email" placeholder="Write your email...">
     </label>
     <label>Password
-        <input type="text">
+        <input type="password" name="pass" placeholder="Write your password...">
     </label>
     <button>LOGIN</button>
     <!-- TODO: Add register button -->
