5661d750e357dcd3a00fd565c2cfa3bb89b516c3
LiamAsensi <liam.alejoasensi@gmail.com>
2024-03-08 04:04:19 +0100

    Finished saveEvent and added deleteEvent

--- public/controller/EventController.php
+++ public/controller/EventController.php
@@ -16,37 +16,79 @@
         exit();
     }
 
-    function saveEvent() {
-        if (
-            empty($_POST['name']) ||
-            empty($_POST['desciption']) ||
-            empty($_POST['province']) ||
-            empty($_POST['locality']) ||
-            empty($_POST['terrainType']) ||
-            empty($_POST['date']) ||
-            empty($_POST['type']) ||
-            empty($_POST['host'])
-        ) {
+    function saveBanner(): string {
+        $fileName = '';
+
+        if (is_uploaded_file($_FILES['bannerPicture']['tmp_name'])) {
+            $uploadDir = dirname(__DIR__) . '/res/images/events/';
+            $uniqueId = time();
+            $fileName = $uniqueId . '-' . $_FILES['bannerPicture']['name'];
+
+            move_uploaded_file($_FILES['bannerPicture']['tmp_name'], $uploadDir . $fileName);
+        } else {
             header(HOME_PATH);
             exit();
         }
 
-        $hostAux = User::getById($_POST['host']);
+        return $fileName;
+    }
 
-        $eventAux = new Event(
-            $_POST['name'], 
-            $_POST['description'], 
-            $_POST['province'], 
-            $_POST['locality'], 
-            $_POST['terrainType'], 
-            $_POST['date'], 
-            $_POST['type'], 
-            User::getById($_POST['host']), 
-            ""
+    function saveEvent() {
+        if (
+            empty($_POST['name']) ||
+            empty($_POST['province']) ||
+            empty($_POST['locality']) ||
+            empty($_POST['date']) ||
+            empty($_POST['host']) ||
+            empty($_POST['type'])
+        ) {
+            // TODO: Redirigir a newEvent con los errores
+            header(HOME_PATH);
+            exit();
+        }
+
+        $name = htmlspecialchars(stripcslashes(trim($_POST['name'])));
+        $description = empty($_POST['description']) ? 'Sin descripción' : htmlspecialchars(stripcslashes(trim($_POST['description'])));
+        $province = htmlspecialchars(stripcslashes(trim($_POST['province'])));
+        $locality = htmlspecialchars(stripcslashes(trim($_POST['locality'])));
+        $terrainType = empty($_POST['terrainType']) ? 'N/A' : htmlspecialchars(stripcslashes(trim($_POST['terrainType'])));
+        $date = htmlspecialchars(stripcslashes(trim($_POST['date'])));
+        $type = htmlspecialchars(stripcslashes(trim($_POST['type'])));
+        $hostId = htmlspecialchars(stripcslashes(trim($_POST['host'])));
+
+        // TODO: Validación específica para date, type y host
+
+        $bannerPicture = '';
+
+        if (!empty($_FILES['bannerPicture'])) {
+            $bannerPicture = saveBanner();
+        }
+
+        $hostAux = User::getById($hostId);
+
+        $eventAux = new Event($name, $description, $province, $locality, $terrainType, new DateTime($date), 
+            $type, $hostAux, $bannerPicture
         );
 
         $eventAux->getAttendees()[] = $hostAux;
         $eventAux->insert();
+
+        header(HOME_PATH);
+        exit();
+    }
+
+    function deleteEvent() {
+        // Checking if there is an ID
+        if (empty($_GET['id'])) {
+            header(HOME_PATH);
+            exit();
+        }
+
+        $eventAux = Event::getById($_GET['id']);
+        $eventAux->delete();
+
+        header(HOME_PATH);
+        exit();
     }
 
     $action = 1;
@@ -61,6 +103,12 @@
             eventDetail();
             break;
         case 3:
+            // Delete an Event
+            deleteEvent();
+            break;
+        case 4:
+            // Inserting an Event
+            saveEvent();
             break;
         default:
             // Displaying Home page
