0fdeb278d3a518bdc921d2c5a95a05ed375294a4
LiamAsensi <liam.alejoasensi@gmail.com>
2024-03-08 23:05:30 +0100

    Changing some of home to better style it

--- public/view/home.php
+++ public/view/home.php
@@ -33,44 +33,42 @@
         <div class="col-xs-12 intro">
             <div class="carousel-inner">
                 <div class="item active">
-                    <img class="img-responsive" src="../res/images/reforesta.png" alt="header picture">
+                    <img class="img-responsive" src="../res/images/reforesta.png" alt="header picture" id="bannerList">
                 </div>
             </div>
         </div>
     </div>
 
     <form action="#" method="get">
-        <div class="col-lg-6">
-            <div class="input-group">
-                <input 
-                    type="text" 
-                    class="form-control" 
-                    name="search" 
-                    placeholder="Buscar por nombre..."
-                    <?php 
-                        if (!empty($_GET['search'])) { 
-                            echo 'value="' . $_GET['search'] . '"';
-                        }
-                    ?>
-                >
-                <span class="input-group-btn">
-                    <button class="btn btn-default" type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
-                </span>
-            </div>
+        <div class="input-group">
+            <input 
+                type="text" 
+                class="form-control" 
+                name="search" 
+                placeholder="Buscar por nombre..."
+                <?php 
+                    if (!empty($_GET['search'])) { 
+                        echo 'value="' . $_GET['search'] . '"';
+                    }
+                ?>
+            >
+            <span class="input-group-btn">
+                <button class="btn btn-default" type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+            </span>
         </div>
     </form>
     
     <main>
-        <h2>Events 
+        <h1>Events 
             <?php if($loggedIn) { ?>
                 <a href="<?= 'http://' . $_SERVER['SERVER_NAME'] . '/view/newEvent.php'?>" class="btn btn-success"><i class="fa fa-plus" aria-hidden="true"></i> Añadir</a>
             <?php } ?>
-        </h2>
+        </h1>
         <div id="eventsContainer">
             <?php
                 // Showing all the events
                 foreach ($events as $event) {
-                    $event->showCard($loggedIn);
+                    $event->showCard();
                 }
 
                 // Printing a message in case there are no events
@@ -80,23 +78,24 @@
             ?>
         </div>
     </main>
-    
-    <!-- Newsletter form -->
-    <div class="index-form text-center">
-        <h3>SUSCRIBETE A NUESTRO NEWSLETTER </h3>
-        <h5>Suscribete para recibir nuestras noticias y regalos</h5>
-        <form class="form-horizontal" method="post" action="<?= $_SERVER['PHP_SELF'] ?>">
-            <div class="form-group">
-                <div class="col-xs-12 col-sm-6 col-sm-push-3 col-md-4 col-md-push-4">
-                    <input class="form-control" type="text" placeholder="Introduce tu email..." name="emailNewsletter">
-                    <button type="submit" name="submit" class="btn btn-lg sr-button">SUSCRIBIRSE</button>
-                    <a href="unsuscribeNewseltter.php" class="btn btn-lg sr-button">DESUSCRIBIRSE</a>
-                </div>
-            </div>
-        </form>
-    </div>
-    <!-- End of Newsletter form -->
 </div>
+
+<!-- Newsletter form -->
+<div class="index-form text-center">
+    <h3>SUSCRIBETE A NUESTRO NEWSLETTER </h3>
+    <h5>Suscribete para recibir nuestras noticias y regalos</h5>
+    <form class="form-horizontal" method="post" action="<?= $_SERVER['PHP_SELF'] ?>">
+        <div class="form-group">
+            <div class="col-xs-12 col-sm-6 col-sm-push-3 col-md-4 col-md-push-4">
+                <input class="form-control" type="text" placeholder="Introduce tu email..." name="emailNewsletter">
+                <button type="submit" name="submit" class="btn btn-lg sr-button">SUSCRIBIRSE</button>
+                <a href="unsuscribeNewseltter.php" class="btn btn-lg sr-button">DESUSCRIBIRSE</a>
+            </div>
+        </div>
+    </form>
+</div>
+<!-- End of Newsletter form -->
+
 <!-- End of index box -->
 <?php
     require_once('footer.php');
